becb787308ee59e5bb4924b935770e940100820f
rajusingha-dar <rajusinghaece@gmail.com>
2025-06-01 00:40:43 +0530

    added tools and created a new agent which can use tool like google search

---
+++ 2_tool_agent/tool_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+
+root_agent = Agent(
+    name= "tool_agent",
+    model = "gemini-2.0-flash",
+    description = "Tool agent",
+    instruction = """
+    You are a helpful assistant that can use the following tools:
+    -google_search""",
+    tools = [google_search]
+)
